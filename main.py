bec2c46077f5555056385d83650dffac8d66962d
root <avijeetdas70@gmail.com>
2022-03-20 20:33:06 +0530

    Added Else

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ if divisor >= 1 or divisor <= -1:
     print("Answer is " +  str(dividend / divisor))
 elif divisor == 0:
     print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+else :
+    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
