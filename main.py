021df9d8bc8139fc07f45128f4b9e5cf0ab4bf2c
root <avijeetdas70@gmail.com>
2022-03-20 20:39:56 +0530

    Final Form

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-dividend = int(input("Enter Dividend : "))
-divisor  = int(input("Enter Divisor  : "))
+from os import system
+while True :
+	system("clear")
+	dividend = int(input("Enter Dividend : "))
+	divisor  = int(input("Enter Divisor  : "))
 
-if divisor >= 1 or divisor <= -1: 
-    print("Answer is " +  str(dividend / divisor))
-elif divisor == 0:
-    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-else :
-    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+	if divisor >= 1 or divisor <= -1: 
+		print("Answer is " +  str(dividend / divisor))
+		input("\nEnter to Continue ")
+	elif divisor == 0:
+		print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+		input("\nEnter to Continue ")
+	else :
+		print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+		input("\nEnter to Continue ")
