8c186f60e005be83782afc8e6d0a304d5c2d3ce6
root <avijeetdas70@gmail.com>
2022-03-20 20:31:41 +0530

    Added Link and If Else

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 dividend = int(input("Enter Dividend : "))
 divisor  = int(input("Enter Divisor  : "))
-print("Answer is " +  str(dividend / divisor))
+
+if divisor >= 1 or divisor <= -1: 
+    print("Answer is " +  str(dividend / divisor))
+elif divisor == 0:
+    print("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
